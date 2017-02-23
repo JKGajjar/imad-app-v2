8b76ad61fad919da8563e4b8fe5675b79e3e51e9
JKGajjar <14it030@charusat.edu.in>
2017-02-23 10:00:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('page-1', function (req, res) {
+    res.send('page 1 is requested for service');
+});
+
+app.get('page-2', function (req, res) {
+    res.send('page 2 is requested for service');
+});
+
+app.get('page-3', function (req, res) {
+    res.send('page 3 is requested for service');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
